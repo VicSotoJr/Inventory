0304f97559e57c6125b991b3467d90e942288508
Victor  Soto <vicsotojr@gmail.com>
2017-11-17 00:05:32 -0500

    seening if the name has anything to do with it

--- login.php
+++ login.php
@@ -7,7 +7,7 @@ if(isset($_SESSION['user'])){
 }
 
 
-require 'dbconnect.php';
+require 'dbConnect.php';
 if(!empty($_POST['username']) && !empty($_POST['password'])):
     $username = $_POST['username'];
     $password = $_POST['password'];
