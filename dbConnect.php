421840043e7b2bdcf8cced96d67b5ba1bc9e6508
Victor  Soto <vicsotojr@gmail.com>
2017-11-16 22:09:37 -0500

    trying to connect to database

--- dbConnect.php
+++ dbConnect.php
@@ -1,17 +1,9 @@
 <?php
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"], 1);
 
-$config = array(
-    'host' => $server ,
-    'user' => $username ,
-    'pw' => $password,
-    'db' => $db
-);
-
+$server = 'us-cdbr-iron-east-05.cleardb.net';
+$user = 'be579759e49bd1';
+$password = 'a623f1e2';
+$database = 'heroku_4dd8172fe7a14c4';
 
 try{
     $conn = mysqli_connect($server, $username, $password, $db);
