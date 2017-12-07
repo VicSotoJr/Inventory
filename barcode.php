53357b805dc82753656760239c496152078ecfa9
Victor  Soto <vicsotojr@gmail.com>
2017-12-07 11:22:14 -0500

    barcode edit

--- barcode.php
+++ barcode.php
@@ -1,5 +1,8 @@
 <?php
  include 'barcode128.php';
+ echo "<br>";
+ echo "<br>";
+ echo "<br>";
  echo '<center><div style="height: 30%; width: 50%;">';
  echo '<p>'.bar128(stripcslashes($_POST['generate'])).'</p>';
  echo '</div></center>';
