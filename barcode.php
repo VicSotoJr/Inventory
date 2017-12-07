7593dd064a33c7d2a4af8c0fb5b56ae27d616b0c
Victor  Soto <vicsotojr@gmail.com>
2017-12-07 11:27:32 -0500

    barcode edit

--- barcode.php
+++ barcode.php
@@ -1,15 +1,6 @@
-
-<script>
-function goBack() {
-    window.history.back();
-}
-</script>
-
-
 <?php
  include 'barcode128.php';
  echo "<br>";
- echo '<button onclick="goBack()">Go Back</button>'
  echo "<br>";
  echo "<br>";
  echo '<center><div style="height: 30%; width: 50%;">';
